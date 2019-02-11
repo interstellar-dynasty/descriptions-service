fc919c3d25647c5763c33ed41e9b7f7e5fcea136
Laura <laura.robertson64@gmail.com>
2019-02-11 15:42:58 -0600

    working on stretch case, factored description to own module

--- public/client/index.jsx
+++ public/client/index.jsx
@@ -2,36 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Axios from 'axios';
 
-class Descriptions extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      title: '',
-      text: ''
-    }
   }
-
-  componentDidMount () {
-    Axios.get('/desc')
-    .then((datas) => {
-      let title = datas.data.title;
-      let text = datas.data.text;
-      this.setState({
-        title: title,
-        text: text
-      })
-    }).catch((err) => console.log('oh no there was an error in Axios request', err))
-  }
-
   render() {
     return (
     <div>
-      <h2>{this.state.title}</h2>
-      <div>{this.state.text}</div>
+      <Descriptions />
     </div>
-    )}
+  )}
 }
 
 ReactDOM.render(
-  <Descriptions />,
+  <App />,
   document.getElementById("desc"));
---
+++ public/client/similarview.jsx
@@ -0,0 +1,3 @@
+//write a customers who viewed this item also viewed
+  //~ 7 other items
+  //stretch case: arrows that allow scrolling to other items in the database
